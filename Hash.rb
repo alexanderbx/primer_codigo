ab74de14045be7ae9995b4307106af532fb049ce
Alx_Bx <cristalexander@gmail.com>
2020-09-11 13:05:02 -0600

    ordenamiento

--- Hash.rb
+++ Hash.rb
@@ -1,33 +1,3 @@
-puts Time.now
-puts ""
-clases = ['mate','ciencia','sociales','politica']
-
-puts "escribir clase"
-
-respuesta = gets.chomp
-
-while !clases.include?(respuesta)
-    puts "escribir de nuevo"
-    respuesta = gets.chomp
-end 
-
-puts "Que quieres responder, casa o edificio?"
-respuesta = gets.chomp
-
-while respuesta != 'casa' && respuesta != 'edificio'
-    puts "escribir nuevamente"
-    respuesta = gets.chomp
-end
-
-if respuesta == "casa"
-    puts "Ok, entonces responde casa"
-elsif respuesta == "edificio"
-    puts "edificio sera"
-end
-
-
-puts "Segunda parte, contesta de forma correcta las preguntas"
-
 preguntas = [
     {'pregunta' => 'De que color es el cielo',
      'respuesta' => '',
